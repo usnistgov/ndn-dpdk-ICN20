79f2228b75dedb05854dc8deaf8c5e4149dd88c1
Junxiao Shi <git@mail1.yoursunny.com>
2019-08-09 17:29:44 +0000

    cpuset: enable cpu_exclusive

--- cpuset.sh
+++ cpuset.sh
@@ -27,6 +27,7 @@ fi
 
 mkdir -p /dev/cpuset/O
 echo $CPU_O >/dev/cpuset/O/cpuset.cpus
+echo 1 >/dev/cpuset/O/cpuset.cpu_exclusive
 cat /dev/cpuset/cpuset.mems >/dev/cpuset/O/cpuset.mems
 for P in $(cat /dev/cpuset/tasks); do
   if readlink /proc/$P/exe >/dev/null; then
@@ -36,5 +37,6 @@ done
 
 mkdir -p /dev/cpuset/B
 echo $CPU_B >/dev/cpuset/B/cpuset.cpus
+echo 1 >/dev/cpuset/B/cpuset.cpu_exclusive
 cat /dev/cpuset/cpuset.mems >/dev/cpuset/B/cpuset.mems
 echo $PROC >/dev/cpuset/B/tasks
